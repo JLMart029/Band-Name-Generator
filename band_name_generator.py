616caefee0dbb714d6c1a473c6578915ff260fd5
JLMart029 <jayleomart@gmail.com>
2025-08-03 13:14:57 -0400

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+name = input("\nWelcome to the Band Name Generator.\n\nWhat is your name?\n")
+fav_activity = input("\nWhat's your favorite activity to do in the summer?\n")
+city = input("\nWhat's the name of the place you like to go to on vacation?\n")
+adjective = input("\nWhat's an adjective you would use to describe your favorite flavor of ice cream? (Taste, color or texture)\n")
+band_name = (name) + " and the " + (adjective) + " " + (fav_activity) + "ers of " + (city)
+print("\nYour band name could be " + (band_name) + "!\nWhat do you think about that, " + (name) + "?")
+
